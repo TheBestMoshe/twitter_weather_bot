41818e2825cb2a5a8bae0f772ae5b5a0fc0828c3
Moshe <mosheygrunwald@gmail.com>
2018-06-27 20:55:15 -0400

    Changed from apscheduler block schedules to background schedules. Added automatic restart if the files have been changed. Added some logs.
    Signed-off-by: Moshe <mosheygrunwald@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3.6
 import parts
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 import logging
 from api_keys import version
 import time
-
-time.sleep(20)
+import os
+import sys
 
 # Logging needs to be completely rewritten
 logger = logging.getLogger(__name__)
@@ -18,29 +18,57 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-sched = BlockingScheduler()
+logger.info('Starting 20 second wait...')
+time.sleep(20)
+logger.info('Wait is complete')
+
+scheduler = BackgroundScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='sun,mon,tue,wed,thu,fri', hour=7)
 def morning_report():
     logger.info('Beginning morning routine')
     logger.info('Sending hourly forecast')
-    parts.tweet(parts.hourly_forecast())
+
+    try:
+        parts.tweet(parts.hourly_forecast())
+    except NameError as e:
+        logger.error(f'Error!\n{e}')
+    except Exception as e:
+        logger.error('Failed to tweet hourly forecast',
+                     f'Exception message:\n{e}')
+
     logger.info('Sending today\'s forecast')
-    parts.tweet(parts.today_weather())
+    try:
+        parts.tweet(parts.today_weather())
+    except NameError as e:
+        logger.error(f'Error!\n{e}')
+    except Exception as e:
+        logger.error('Failed to tweet today weather forecast'
+                     f'Exception message:\n{e}')
 
 
-@sched.scheduled_job('cron', day_of_week='sat,mon,thu', hour=20)
 def evening_report():
     logger.info('sending weekly forecast')
     parts.tweet(parts.weekly_forecast())
 
 
-# @sched.scheduled_job('interval', minutes=1)
-# def minutely_report():
-#     parts.tweet(parts.hourly_forecast(1))
-#     print('hourly_forecast ran')
+scheduler.add_job(morning_report, trigger='cron', day_of_week='mon-fri, sun', hour=7)
 
+# Send me a DM letting me know that the bot instance has started
 parts.dm('moshe_grunwald', f'{version} version of LkwdWeatherBot has been started')
 logger.info('Starting scheduler')
-sched.start()
+scheduler.start()
+
+# Once we started the scheduler, we will begin checking if the script has been changed
+cwd = os.getcwd() + '/'
+watched_files = [__file__, cwd+'parts.py', cwd+'api_keys.py']
+
+watched_files_mtimes = [(f, os.path.getmtime(f)) for f in watched_files]
+
+while True:
+    for f, mtime in watched_files_mtimes:
+        if os.path.getmtime(f) != mtime:
+            os.execv(__file__, sys.argv)
+
+        else:
+            time.sleep(1)
