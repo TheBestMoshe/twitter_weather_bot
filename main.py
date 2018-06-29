347f1ccc6f8afce056087d3a2c43fee08103bd5d
Moshe <mosheygrunwald@gmail.com>
2018-06-29 11:12:56 -0400

    Fix bug where script would crash when running on server.
    Replaced 20 second startup delay with a check to see if there is a
    network connection.
    
    Signed-off-by: Moshe <mosheygrunwald@gmail.com>

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from api_keys import version
 import time
 import os
 import sys
+import requests
 
 # Logging needs to be completely rewritten
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 handler = logging.FileHandler('logs.log')
 handler.setLevel(logging.INFO)
@@ -18,10 +19,22 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-logger.info('Starting 20 second wait...')
-time.sleep(20)
-logger.info('Wait is complete')
 
+network_connection = False
+connection_attempts = 0
+while not network_connection:
+    try:
+        connection_attempts += 1
+        logging.info('Testing network connection')
+        requests.get('https://google.com')
+        network_connection = True
+        logging.info(f'It took {connection_attempts/2} seconds to connect to network.')
+    except Exception:
+        logging.info('Not connected to the network')
+        time.sleep(2)
+
+
+# Create a scheduler
 scheduler = BackgroundScheduler()
 
 
@@ -52,7 +65,9 @@ def evening_report():
     parts.tweet(parts.weekly_forecast())
 
 
+# Schedule jobs
 scheduler.add_job(morning_report, trigger='cron', day_of_week='mon-fri, sun', hour=7)
+
 
 # Send me a DM letting me know that the bot instance has started
 parts.dm('moshe_grunwald', f'{version} version of LkwdWeatherBot has been started')
@@ -60,15 +75,16 @@ logger.info('Starting scheduler')
 scheduler.start()
 
 # Once we started the scheduler, we will begin checking if the script has been changed
-cwd = os.getcwd() + '/'
-watched_files = [__file__, cwd+'parts.py', cwd+'api_keys.py']
+cwd = os.path.abspath(os.path.dirname(__file__)) + '/'
+watched_files = [sys.argv[0], cwd + 'parts.py', cwd + 'api_keys.py']
 
 watched_files_mtimes = [(f, os.path.getmtime(f)) for f in watched_files]
 
 while True:
     for f, mtime in watched_files_mtimes:
         if os.path.getmtime(f) != mtime:
-            os.execv(__file__, sys.argv)
+            logging.info('Found changes in the files. Restarting...')
+            os.execv(sys.argv[0], sys.argv)
 
         else:
             time.sleep(1)
