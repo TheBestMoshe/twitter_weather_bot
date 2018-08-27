b15348106db395fe3ee6f52615464ff48706d418
Moshe <mosheygrunwald@gmail.com>
2018-08-27 14:51:17 -0400

    Cleaned up code to be PEP8 complient

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ while not network_connection:
         if connection_attempts == 1:
             logger.info('Connected to network')
         else:
-            logger.info(f'It took {connection_attempts/2} seconds to connect to network.')
+            logger.info(f'It took {connection_attempts/2} seconds to connect'
+                        ' to network.')
     except Exception:
         logger.info('Not connected to the network')
         time.sleep(2)
@@ -51,7 +52,7 @@ def morning_report():
     except NameError as e:
         logger.error(f'Error!\n{e}')
     except Exception as e:
-        logger.error('Failed to tweet hourly forecast',
+        logger.error('Failed to tweet hourly forecast' +
                      f'Exception message:\n{e}')
 
     logger.info('Sending today\'s forecast')
@@ -70,21 +71,26 @@ def evening_report():
 
 
 # Schedule jobs
-scheduler.add_job(morning_report, trigger='cron', day_of_week='mon-fri, sun', hour=7)
-scheduler.add_job(evening_report, trigger='cron', day_of_week='sun, wed', hour=20)
-scheduler.add_job(evening_report, trigger='cron', day_of_week='fri', hour=14)
+scheduler.add_job(morning_report, trigger='cron',
+                  day_of_week='mon-fri, sun', hour=7)
+scheduler.add_job(evening_report, trigger='cron',
+                  day_of_week='sun, wed', hour=20)
+scheduler.add_job(evening_report, trigger='cron',
+                  day_of_week='fri', hour=14)
 
 
 # Send me a DM letting me know that the bot instance has started
 logger.debug('Sending startup DM')
-parts.dm('moshe_grunwald', f'{version} version of LkwdWeatherBot has been started')
+parts.dm('moshe_grunwald', f'{version} version of LkwdWeatherBot has been '
+         'started')
 logger.info('Sent startup DM')
 
 logger.debug('Starting scheduler')
 scheduler.start()
 logger.info('Scheduler started')
 
-# Once we started the scheduler, we will begin checking if the script has been changed
+# Once we started the scheduler, we will begin checking if the script has been
+#   changed
 cwd = os.path.abspath(os.path.dirname(__file__)) + '/'
 watched_files = [sys.argv[0], cwd + 'parts.py', cwd + 'api_keys.py']
 
@@ -94,9 +100,9 @@ while True:
     for f, mtime in watched_files_mtimes:
         if os.path.getmtime(f) != mtime:
             logger.info(f'Found changes in {f}.\nRestarting...')
-            # Added this sleep to make sure all the files have been writen to.
-            # In the past the script would restart while the files were being overwritten
-            # it would crash
+            # Added this sleep to make sure all the files have been written to.
+            # In the past the script would restart while the files were being
+            # overwritten it would crash
             time.sleep(2)
             os.execv(sys.argv[0], sys.argv)
         else:
